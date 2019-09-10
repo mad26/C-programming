0d953fffad14166ea5d371b80e6d4601c64b604a
Madhav Khindri <madavkhindri@gmail.com>
2019-09-11 00:37:18 +0530

    Update and rename 3. printf_output_types.c to 3.1 printf_output_types.c

--- 3. printf_output_types.c
+++
@@ -1,6 +0,0 @@
-int main()
-{
-  int a=4;
-  printf("%d",a); //%d is a format specifier
-  return 0;
-}
---
+++ 3.1 printf_output_types.c
@@ -0,0 +1,10 @@
+int main()
+{
+  int a=4, b=5;
+  printf("%d",a); //%d is a format specifier
+  printf("value of a is %d", a); // printing value of variable
+  printf("value of a is %d and b is %d", a, b);
+  printf("sum of %d and %d is %d",a,b,a+b);  // printing value of expression
+  return 0;
+}
+
